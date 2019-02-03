c876bd9028706903b1303b7a4d82424ba82ec4c8
Ludovic Agathe <ludovic.agathe@gmail.com>
2019-02-04 00:57:09 +0400

    20190204 - night

---
+++ maximilian - s8/serve.bat
@@ -0,0 +1,2 @@
+call npm run serve
+pause
---
+++ maximilian - s8/src/components/Child.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="child">
+    <h4>{{ msg }}</h4>
+    <div>From parent: {{ randomNum }}</div>
+    <div><button @click="pingParent()">Ping</button></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Child',
+  props: {
+    msg: String,
+    refId: String
+  },
+  data: function() {
+    return{
+      randomNum: 0
+    }
+  },
+  methods: {
+    pingParent: function() {
+      this.$emit("childPing", this.refId);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
