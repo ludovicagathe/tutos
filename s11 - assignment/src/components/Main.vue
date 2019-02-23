867cee282094e86cbade89807fde9aa5ee9ac8b8
Ludovic Agathe <ludovic.agathe@gmail.com>
2019-02-23 18:09:45 +0400

    20190223 - afternoon

--- s11 - assignment/src/components/Main.vue
+++ s11 - assignment/src/components/Main.vue
@@ -1,96 +1,80 @@
 <template>
-  <v-container grid-list-lg>
-    <v-layout
-      text-xs-center
-      wrap
-    >
-      <v-flex xs6 mb-5>
-        <v-form>
-          <v-toolbar dark color="blue" py-2>
-            <v-toolbar-title>Form</v-toolbar-title>
-          </v-toolbar>
-          <v-card>
-            <div class="px-5 pt-2">
-              <v-text-field label="email" id="email" @change="email_fn" my-0></v-text-field>
-              <v-text-field label="password" type="password" required @change="passwd_fn"></v-text-field>
-              <v-text-field label="age" type="number" @change="age_fn" suffix="years" min="50" step="4" class="mb-2"></v-text-field>
-              <v-textarea solo clearable auto-grow counter="140" rows="3" class="pb-3 mb-2" @change="message_fn" @blur="message_fn"></v-textarea>
-              <v-layout row>
-                <v-flex xs6><v-checkbox label="Send mail" @change="send_mail($event, 'sendmail')"></v-checkbox></v-flex>
-                <v-flex xs6><v-checkbox label="Send info mail" @change="send_mail($event, 'sendinfomail')"></v-checkbox></v-flex>
-              </v-layout>
-              <v-layout>
-                <v-radio-group v-model="sex" row>
-                  <v-radio label="male" value="male"></v-radio>
-                  <v-radio label="female" value="female"></v-radio>
-                </v-radio-group>
-              </v-layout>
-              <v-layout>
-                <v-flex xs12>
-                  <v-select :items="Object.keys(priority)" label="Priority" solo mb-3 v-model="status"></v-select>
-                </v-flex>
-              </v-layout>
-            </div>
-          </v-card>
-        </v-form>
+  <v-container grid-list-lg justify-center>
+    <v-layout>
+      <v-flex xs6 offset-xs3 v-if="!submitted">
+        <v-card>
+          <v-card-title class="red text-xs-center" dark primary-title>
+            <h3 class="grow">Registration</h3>
+          </v-card-title>
+          <v-card-text>
+            <v-form>
+              <v-text-field label="First Name" v-model="first_name" solo mb-2></v-text-field>
+              <v-text-field label="Last Name" v-model="last_name" solo mb-2></v-text-field>
+              <v-text-field label="Full Name" :value="full_name" disabled solo mb-2></v-text-field>
+              <v-text-field label="e-mail" type="email" v-model="email" solo mb-2></v-text-field>
+              <v-text-field label="Password" type="password" v-model="password" solo mb-2></v-text-field>
+              <v-switch v-model="save" :label="'Save data? ' + (save ? 'Yes' : 'No')" mb-2></v-switch>
+              <v-btn color="success" @click="submit">Submit</v-btn>
+            </v-form>
+          </v-card-text>
+        </v-card>
       </v-flex>
 
-      <v-flex xs6 mb-5 shrink>
-        <div id="controls">
-          <v-btn @click="submit" v-if="!submitted" color="success">submit</v-btn>
-        </div>
-        <div id="results" v-if="submitted" >
-          <v-toolbar dark color="pink" py-2>
-            <v-toolbar-title>Results</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn @click="submitted = !submitted" color="error">clear</v-btn>
-          </v-toolbar>
+      <v-flex xs6 offset-xs3 v-if="submitted">
+        <v-card>
+          <v-card-title class="blue text-xs-center" dark primary-title>
+            <h3 class="grow">Your Submission</h3>
+          </v-card-title>
+          <v-card-text>
 
-          <v-card>
-            <v-layout px-5 pt-2 mb-2>
-              <v-flex xs3 offset-xs1>
-                e-mail:
+            <v-layout row wrap>
+              <v-flex xs6 text-xs-right>
+                <h4>First Name</h4>
               </v-flex>
-              <v-flex xs7>{{ email }}</v-flex>
-            </v-layout>
-            <v-layout px-5 pt-2 mb-2>
-              <v-flex xs3 offset-xs1>
-                Password:
+              <v-flex xs6 text-xs-left>
+                {{ first_name }}
               </v-flex>
-              <v-flex xs7>{{ passwd }}</v-flex>
-            </v-layout>
-            <v-layout px-5 pt-2 mb-2>
-              <v-flex xs3 offset-xs1>
-                Age:
+
+              <v-flex xs6 text-xs-right>
+                <h4>Last Name</h4>
               </v-flex>
-              <v-flex xs7>{{ age }}</v-flex>
-            </v-layout>
-            <v-layout px-5 pt-2 mb-2>
-              <v-flex xs3 offset-xs1>
-                Message:
+              <v-flex xs6 text-xs-left>
+                {{ last_name }}
               </v-flex>
-              <v-flex xs7 text-output>{{ message }}</v-flex>
-            </v-layout>
-            <v-layout px-5 pt-2 mb-2>
-              <v-flex xs3 offset-xs1>
-                Sex:
+
+              <v-flex xs6 text-xs-right>
+                <h4>First Name</h4>
               </v-flex>
-              <v-flex xs7>{{ sex }}</v-flex>
-            </v-layout>
-            <v-layout px-5 pt-2 v-for="(option, key) in mail_options" :key="option.id" mb-2>
-              <v-flex xs3 offset-xs1>
-                Send mail option ({{ key }}) :
+              <v-flex xs6 text-xs-left>
+                {{ full_name }}
               </v-flex>
-              <v-flex xs7>{{ option }}</v-flex>
-            </v-layout>
-            <v-layout pt-2 mb-2 style="margin: auto 0px;">
-              <v-flex justify-center xs12 dark :style="{ 'background-color': priority[status], 'color': 'white', 'margin': 'auto 0'}" pa-3>
-                <div>{{ status }}</div>
+
+              <v-flex xs6 text-xs-right>
+                <h4>e-mail</h4>
               </v-flex>
+              <v-flex xs6 text-xs-left>
+                {{ email }}
+              </v-flex>
+
+              <v-flex xs6 text-xs-right>
+                <h4>Password</h4>
+              </v-flex>
+              <v-flex xs6 text-xs-left>
+                {{ password }}
+              </v-flex>
+
+              <v-flex xs6 text-xs-right>
+                <h4>Save Data</h4>
+              </v-flex>
+              <v-flex xs6 text-xs-left>
+                {{ (save ? 'Yes' : 'No') }}
+              </v-flex>
+
             </v-layout>
-          </v-card>
-        </div>
+          </v-card-text>
+        </v-card>
       </v-flex>
+
     </v-layout>
   </v-container>
 </template>
@@ -98,52 +82,28 @@
 <script>
   export default {
     data: () => ({
+      first_name: "",
+      last_name: "",
+      save: false,
       email: "",
-      passwd: "",
-      age: 0,
-      message: "",
-      mail_options: { sendmail: false, sendinfomail: false },
-      sex: '',
-      priority: {
-        'Critical':'crimson',
-        'Severe':'lightcoral',
-        'Serious':'pink',
-        'Medium':'lightblue',
-        'Low':'turquoise',
-        'Insignificant':'lime'
-      },
-      status: 'Medium',
+      password: "",
       submitted: false
     }),
     methods: {
-      email_fn(value) {
-        this.email = value;
-      },
-      passwd_fn(value) {
-        this.passwd = value;
-      },
-      age_fn(value) {
-        this.age = value;
-      },
-      message_fn(value) {
-        if(typeof value == 'string') {
-          this.message = value;
-        } else {
-          this.message = value.target.value;
-        }
-      },
-      send_mail(ev, index) {
-        this.mail_options[index] = !this.mail_options[index];
-      },
       submit() {
-        this.submitted = true;
+        this.submitted = !this.submitted;
+      }
+    },
+    computed:{
+      full_name() {
+        return this.first_name + ' ' + this.last_name;
       }
     }
   }
 </script>
 
 <style>
-.text-output {
-  white-space: pre;
+.red h3 {
+  color: white;
 }
 </style>
