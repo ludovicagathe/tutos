30bb1621d000a73428c876c98944e96e003a3acc
Ludovic Agathe <ludovic.agathe@gmail.com>
2019-02-25 16:00:44 +0400

    20190225 - afternoon

--- s14 - course/src/components/Main.vue
+++ s14 - course/src/components/Main.vue
@@ -10,6 +10,9 @@
         <transition name="fade">
           <div class="pink accent-2 teal--text text--darken-2 font-weight-bold mb-2 pa-2" v-if="visible">This is toggled</div>
         </transition>
+        <transition name="slide">
+          <div class="pink accent-2 teal--text text--darken-2 font-weight-bold mb-2 pa-2" v-if="visible">This is toggled</div>
+        </transition>
       </v-flex>
 
     </v-layout>
@@ -37,5 +40,36 @@ export default {
 .fade-leave-active {
   transition: opacity 1s;
   opacity: 0;
+}
+.slide-enter {
+  transform: translateY(50px);
+}
+.slide-enter-active {
+  animation: slidein 1s ease-in 1 forwards;
+}
+.slide-leave {
+
+}
+.slide-leave-active {
+  transform: translateY(50px);
+  animation: slideout 1s ease-out 1 forwards;
+}
+
+@keyframe slidein {
+  0% {
+    transform: translateY(50px);
+  }
+  100% {
+    transform: translateY(0px);
+  }
+}
+
+@keyframe slideout {
+  0% {
+    transform: translateY(0px);
+  }
+  100% {
+    transform: translateY(50px);
+  }
 }
 </style>
