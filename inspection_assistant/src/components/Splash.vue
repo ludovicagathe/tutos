9b91d3d989166c3f4383a26c77f2e751578115f1
Ludovic Agathe <ludovic.agathe@gmail.com>
2020-01-30 23:34:18 +0400

    20200130 - night

---
+++ inspection_assistant/src/components/Splash.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" fill-height>
+        <v-row align="center">
+          <v-img
+            :src="require('../assets/eemo_logo.png')"
+            class="my-3"
+            contain
+            height="300"
+          ></v-img>
+        </v-row>
+        <v-spacer></v-spacer>
+        <v-row justify="center">
+          <v-btn color="success" height="80px" class="title" width="60%">Start</v-btn>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Splash',
+
+  data: () => ({
+
+  }),
+};
+</script>
--- inspection_assistant/src/views/Home.vue
+++ inspection_assistant/src/views/Home.vue
@@ -1,15 +1,27 @@
 <template>
-  <HelloWorld />
+  <v-container fill-height>
+    <v-row fill-height>
+      <v-col cols="12">
+        <Splash />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld';
+import Splash from '../components/Splash';
 
 export default {
   name: 'Home',
 
   components: {
-    HelloWorld,
+    Splash,
   },
 };
 </script>
+
+<style>
+#app {
+  background: radial-gradient(50% 50% at 50% 50%, #FFFFFF 0%, #ECF1FF 100%)
+}
+</style>
