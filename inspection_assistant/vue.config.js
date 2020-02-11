4bc68b0c6e5bfcf2ba7ceef0305c27be8e6e47a3
Ludovic Agathe <ludovic.agathe@gmail.com>
2020-02-12 00:51:42 +0400

    20200211 - night

--- inspection_assistant/vue.config.js
+++ inspection_assistant/vue.config.js
@@ -2,5 +2,20 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  publicPath: ''
+  baseUrl: '',
+  pages: {
+    index: {
+      entry: './src/main.js',
+      template: './public/index.html',
+      title: 'Inspection Assistant',
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+    config: {
+      entry: './src/pages/Config/main.js',
+      template: './public/config.html',
+      title: 'Inspection Assistant - Config',
+      filename: 'config.html',
+      chunks: ['chunk-vendors', 'chunk-common', 'config']
+    },
+  }
 }
