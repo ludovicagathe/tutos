f86ae1d0eb02a1a4d6ce61b9670e5814eb9a282f
Ludovic Agathe <ludovic.agathe@gmail.com>
2022-07-14 14:54:29 +0400

    20220714 - afternoon

---
+++ Shell Scripting/tt-default-git-commit-string.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git pull
+git add .
+git commit -am $(date +"%Y%m%d")" - "$(if [ $(date +"%H") -gt 21 -o $(date +"%H") -le 5 ];then echo "night";fi;)$(if [ $(date +"%H") -gt 5 -a $(date +"%H") -le 11 ];then echo "morning";fi;)$(if [ $(date +"%H") -gt 11 -a $(date +"%H") -le 17 ];then echo "afternoon";fi;)$(if [ $(date +"%H") -gt 17 -a $(date +"%H") -le 21 ];then echo "evening";fi;)
+git push
