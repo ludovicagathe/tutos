11d74dc823d4b60cd26fbc2e30d769837609c4e6
Ludovic Agathe <ludovic.agathe@gmail.com>
2022-07-27 01:15:12 +0400

    20220727 - night

---
+++ Coding Garden/Travel Log/server/src/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const helmet = require('helmet');
+const fs = require('fs');
+const path = require('path');
+const cors = require('cors');
+
+// middlewares
+const middlewares = require('./middlewares');
+
+
+// setup
+const port = process.env.PORT || 3000;
+const logging = process.env.LOGGING || 'common';
+
+// logging
+const accessLogStream = fs.createWriteStream(path.resolve(__dirname, '..', 'logs', 'access.log'), { flags: 'a' });
+app.use(morgan(logging, { stream: accessLogStream }));
+
+// headers
+app.use(helmet());
+app.use(cors({
+  origin: 'http://localhost:3000'
+}))
+
+// routes
+app.get('/', (req, res) => {
+  res.json({ msg: "OK" });
+});
+
+// errors
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+app.listen(port, () => { console.log(`Listening on port ${port}`) })
